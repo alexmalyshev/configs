df13329376d8b9e456b29e5e313a222953f76e46
Alexander Malyshev <lex.malyshev@gmail.com>
2016-08-03 14:59:13 -0700

    [bashrc] Remove long-unused gcc and clang aliases

--- aliases.bash
+++ aliases.bash
@@ -5,28 +5,6 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias mv='mv -i'
 
-# Quick ways of setting flags for GCC.
-alias gccb='gcc -std=c99 -pedantic -Wall -Wextra'
-alias gccf='gccb -fsyntax-only'
-alias gccd='gccb -g -fsanitize=address'
-alias gcco='gccb -O3'
-
-alias g++b='g++ -std=c++11 -pedantic -Wall -Wextra'
-alias g++f='g++b -fsyntax-only'
-alias g++d='g++b -g -ftrapv -fsanitize=address'
-alias g++o='g++b -O3'
-
-# Ditto for clang.
-alias clangb='clang -std=c99 -pedantic -Wall -Wextra'
-alias clangf='clangb -fsyntax-only'
-alias clangd='clangb -g -ftrapv -fsanitize=address'
-alias clango='clangb -O3'
-
-alias clangb++='clang++ -std=c++11 -pedantic -Wall -Wextra'
-alias clangf++='clangb++ -fsyntax-only'
-alias clangd++='clangb++ -g -ftrapv -fsanitize=address'
-alias clango++='clangb++ -O3'
-
 # Get rid of the default GUI for emacs.
 alias e='emacs -nw'
 alias emacs='emacs -nw'
