05cdfe075e26e4de6f8251e41e427c6f4ebc6d35
Alexander Malyshev <lex.malyshev@gmail.com>
2018-01-05 13:52:15 -0500

    [emacs] Move emacs over to emacs.d/init.el
    And update the installer to handle files in emacs.d

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ "${1}" = '-f' ]; then
   FORCE=1
 fi
 
-CONFIGS=$(ls | grep -v -e install -e README)
+CONFIGS=$(ls | grep -v -e install -e README -e emacs.d)
 
 LN_ARGS='-s'
 if [ -n "${FORCE}" ]; then
@@ -16,4 +16,12 @@ fi
 
 for CONFIG in ${CONFIGS}; do
   ln "${LN_ARGS}" "$(realpath ${CONFIG})" "${HOME}/.${CONFIG}"
+done
+
+TARGET="${HOME}/.emacs.d"
+mkdir -p "${TARGET}"
+
+EMACS=$(ls emacs.d)
+for EMAC in ${EMACS}; do
+  ln "${LN_ARGS}" "$(realpath emacs.d/${EMAC})" "${TARGET}/${EMAC}"
 done
